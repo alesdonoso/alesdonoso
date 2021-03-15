ff33249a891104296a165ffeca66abace5dd0242
alesdonoso <aledonosan@gmail.com>
2021-03-15 18:48:10 +0100

    added folder for work

--- components/workData.js
+++ components/workData.js
@@ -5,7 +5,7 @@ const workData = [
     title: "Card 1",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    route: "/work",
+    route: "/work/abtest",
   },
   {
     title: "Card 2",
---
+++ pages/work/abtest.js
@@ -0,0 +1,8 @@
+
+function abtest(){
+    return (
+        <h1>Hola mundo</h1>
+    )
+}
+
+export default abtest;
